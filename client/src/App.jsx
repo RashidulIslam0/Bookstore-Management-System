79581c9c67cead3cfc656ce6d88315ac30ebb314
Rashidul <aboutrashidul@gmail.com>
2024-01-19 22:52:53 +0600

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import 'Routes' in addition to 'Route'
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import Books from "./Components/Books";
+import Login from "./Components/Login";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import "../css/navbar.css";
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <>
+      <nav className="navbar ">
+        <div className="navbar-left">
+          <Link className="navbar-brand" to="/">
+            Book Store
+          </Link>
+        </div>
+
+        <div className=" navbar-right">
+          <Link className="navbar-link" to="/books">
+            Book Store
+          </Link>
+          <Link className="navbar-link" to="/login">
+            Login
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 };
 
